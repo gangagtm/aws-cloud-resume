9ff382dd3a30daa0243cebbcc51290e91d7f5ba6
Ganga Gautam <gangagautam@Gangas-MacBook-Pro.local>
2024-02-26 22:15:21 -0600

    Initial commit

---
+++ Portfolioreal/assets/js/index.js
@@ -0,0 +1,7 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+        let response = await fetch("https://io7dzcpsyuyapbzomibke6cmou0cawlc.lambda-url.us-east-1.on.aws/");
+        let data = await response.json();
+        counter.innerHTML = ` Views: ${data}`;
+}
+updateCounter();
